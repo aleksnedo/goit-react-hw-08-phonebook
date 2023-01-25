849ede75bc9ea3180bac6598e70740e32c3d68c0
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2023-01-25 10:36:14 +0200

    Update AppBar.styled.jsx

--- src/components/AppBar/AppBar.styled.jsx
+++ src/components/AppBar/AppBar.styled.jsx
@@ -1,19 +1,9 @@
 import styled from '@emotion/styled';
 
-// export const Wrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-//   gap: 12px;
-// `;
-
-// export const UserName = styled.p`
-//   font-weight: 700;
-// `;
-
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin-bottom: 16px;
-  border-bottom: 1px solid #2a363b;
+  border-bottom: 2px solid #1976d2;
 `;
